40b8314e42489c1bbb37e9996a7e4857f329b8ba
DanielKonjarski <daniel.konjarski@georgebrown.ca>
2025-03-16 19:52:03 -0400

    Created all style files

---
+++ src/styles/theme.js
@@ -0,0 +1,14 @@
+const theme = {
+    colors: {
+      background: "#121212",
+      primary: "#d32f2f",  // Red
+      secondary: "#1f1f1f", // Dark gray
+      text: "#ffffff",
+    },
+    fonts: {
+      main: "'Poppins', sans-serif",
+    },
+  };
+  
+  export default theme;
+  
