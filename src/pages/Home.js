41153fdaf089ee1be75c3b1dd3155f4d81fac383
DanielKonjarski <daniel.konjarski@georgebrown.ca>
2025-03-16 23:09:20 -0400

    Updated home.js file

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
       <section className="bio">
         <h2>About Me</h2>
         <p>
-          Hello, I'm Daniel, a dedicated software developer with a strong foundation in both front-end and back-end technologies. I specialize in building scalable, efficient, and user-centric applications with a focus on React, Node.js, and Swift. My approach to development is driven by a passion for clean code, problem-solving, and delivering high-quality digital solutions that meet both user and business needs.
+          Hello, I'm Daniel, a dedicated software developer with a strong foundation in both front-end and back-end technologies. I specialize in building scalable, efficient, and user-centric applications with a focus on React, Node.js, and C#. My approach to development is driven by a passion for clean code, problem-solving, and delivering high-quality digital solutions that meet both user and business needs.
         </p>
         <p>
           I bring a combination of technical expertise, attention to detail, and collaborative teamwork to every project. I am committed to continuous growth and staying current with emerging technologies to ensure the solutions I create are innovative and sustainable.
