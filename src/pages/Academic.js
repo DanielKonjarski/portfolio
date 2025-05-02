ad760354345c2fbaaf8e99e37129f10455ee286d
DanielKonjarski <danielkonjo@hotmail.com>
2025-05-02 13:48:43 -0400

    Added Travel Agecny Description

--- src/pages/Academic.js
+++ src/pages/Academic.js
@@ -114,7 +114,7 @@ const Academic = () => {
             className="academic-img"
           />
           <h3>Travel Agency</h3>
-          <p>This is a short description of the project.</p>
+          <p>Travel agenct application using MVC architecture and written in C#.</p>
           <a
             href="https://github.com/DanielKonjarski/travel-agency"
             target="_blank"
