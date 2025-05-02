3b9d58228ffd98ea8ea2b5a601e4d496d9fed489
DanielKonjarski <danielkonjo@hotmail.com>
2025-05-02 13:09:41 -0400

    Removed import

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import PersonalData from "./pages/PersonalData";
 import Academic from "./pages/Academic";
 import WorkExperience from "./pages/WorkExperience";
 import Professional from "./pages/Professional";
-import Details from "./pages/Details/DetailsOne";
 
 function App() {
   return (
