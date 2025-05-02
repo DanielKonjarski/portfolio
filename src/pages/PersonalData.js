c30f8ab5e900dfdc3447da446e2a3488c1ad5445
DanielKonjarski <danielkonjo@hotmail.com>
2025-05-01 22:57:28 -0400

    Added Cover Letter

--- src/pages/PersonalData.js
+++ src/pages/PersonalData.js
@@ -1,12 +1,24 @@
 import React from "react";
 
 const PersonalData = () => {
-  return (
-    <div>
-      <h1>Personal Data</h1>
-      <p>Bio, Resume, Cover Letter, and Career Philosophy.</p>
-    </div>
-  );
+    return (
+        <div className="resume-container" style={{ textAlign: "center", padding: "20px" }}>
+          <h1>Resume</h1>
+          <embed
+            src={require("../assets/Resume.pdf")}
+            type="application/pdf"
+            width="100%"
+            height="800px"
+          />
+          <h1>Cover Letter</h1>
+          <embed
+            src={require("../assets/Cover Letter.pdf")}
+            type="application/pdf"
+            width="100%"
+            height="800px"
+          />
+        </div>
+      );
 };
 
 export default PersonalData;
